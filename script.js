fb72ddb100e0ae7a9da4c831fce3d94b150ff7d0
doko1414 <javierlarasolis1414@gmail.com>
2025-02-24 19:23:32 -0400

    --

--- script.js
+++ script.js
@@ -3,3 +3,38 @@ const board=document.getElementById('board');
 const scoreBoard=document.getElementById('score-board');
 const startButtom=document.getElementById('start');
 const gameOverSign=document.getElementById('game-over');
+
+//Game Settings
+const boardSize=10;
+const gameSpeed=100;
+const squareTypes={//tipos de cuadrados
+  emptySquare:0,//cuadrados vacios 
+  snakeSquare:1,//cuadrados de la serpiente
+  foodSquare:2// cuadrados de la comida
+};
+const directions={
+  ArrowUp:-10,
+  ArrowDown:10,
+  ArrowRight:1,
+  ArrowLeft:-1,
+}
+//Game variables
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const setGame=()=>{
+  snake=['00','01','02','03'];//creamos la serpiente
+  score=snake.length;
+  direction='ArrowRight';
+  boardSquares=Array.from(Array(boardSize),()=>new Array(boardSize).fill(squareTypes.emptySquare))
+};
+
+const startGame=()=>{
+  setGame();//lo primero es setear los valores de las variables para comenzar el juego
+};
+
+startButtom.addEventListener('click',startGame)
