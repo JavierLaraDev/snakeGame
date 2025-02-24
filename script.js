90c5bccd8cf42f083d5d44d5455eeba6da086c0a
JavierLSDev <javierlarasolis79@gmail.com>
2025-02-24 18:23:55 -0400

    game at 50%

---
+++ script.js
@@ -0,0 +1,5 @@
+//primero tomaremos todos los elementos html que necesitamos
+const board=document.getElementById('board');
+const scoreBoard=document.getElementById('score-board');
+const startButtom=document.getElementById('start');
+const gameOverSign=document.getElementById('game-over');
